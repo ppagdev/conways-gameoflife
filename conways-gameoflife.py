c0ce36c2ee02230e64537bc95a011c6d6cab60a4
Ppag <ppagdev@tuta.com>
2023-12-19 15:58:47 +0000

    updated conways-gameoflife.py

--- conways-gameoflife.py
+++ conways-gameoflife.py
@@ -22,13 +22,11 @@ for x in range(0,windowWidth,blockSize):
     grid.append([])
     for y in range(0, windowHeight, blockSize):
         grid[count].append("dead")
-print(len(grid))
-print(len(grid[14]))
 
-#test
-grid[3][4] = "alive"
-
-print(grid[4][5])
+#debug
+#print(len(grid))
+#print(len(grid[14]))
+#print(grid[4][5])
 
 def drawGrid():
     #range(start,stop,incrementby)
@@ -57,8 +55,132 @@ def toggleBlock(mousex, mousey):
     else:
         print("something's wrong")
 
+def getLiveNeighbourCount(x,y):
+    liveNeighbourCount = 0
+    xlength = len(grid)-1
+    ylength = len(grid[0])-1
+    print(x,y)
+    #specific cases
+    #x=0
+    if x == 0:
+        if y == 0:
+            if grid[x][y+1] == "alive": #down
+                liveNeighbourCount += 1
+            if grid[x+1][y] == "alive": #right
+                liveNeighbourCount += 1
+            if grid[x+1][y+1] == "alive": #down right
+                liveNeighbourCount += 1
+        elif y == ylength:
+            if grid[x][y-1] == "alive": #up
+                liveNeighbourCount += 1
+            if grid[x+1][y] == "alive": #right
+                liveNeighbourCount += 1
+            if grid[x+1][y-1] == "alive": #up right
+                liveNeighbourCount += 1
+        else:
+            if grid[x][y-1] == "alive": #up
+                liveNeighbourCount += 1
+            if grid[x][y+1] == "alive": #down
+                liveNeighbourCount += 1
+            if grid[x+1][y] == "alive": #right
+                liveNeighbourCount += 1
+            if grid[x+1][y-1] == "alive": #up right
+                liveNeighbourCount += 1
+            if grid[x+1][y+1] == "alive": #down right
+                liveNeighbourCount += 1
+        return liveNeighbourCount
 
+    #x=max
+    if x == xlength:
+        if y == 0:
+            if grid[x-1][y] == "alive": #left
+                liveNeighbourCount += 1
+            if grid[x][y+1] == "alive": #down
+                liveNeighbourCount += 1
+            if grid[x-1][y+1] == "alive": #down left
+                liveNeighbourCount += 1
+        if y == ylength:
+            if grid[x][y-1] == "alive": #up
+                liveNeighbourCount += 1
+            if grid[x-1][y] == "alive": #left
+                liveNeighbourCount += 1
+            if grid[x-1][y-1] == "alive": #up left
+                liveNeighbourCount += 1
+        else:
+            if grid[x][y-1] == "alive": #up
+                liveNeighbourCount += 1
+            if grid[x][y+1] == "alive": #down
+                liveNeighbourCount += 1
+            if grid[x-1][y] == "alive": #left
+                liveNeighbourCount += 1
+            if grid[x-1][y-1] == "alive": #up left
+                liveNeighbourCount += 1
+            if grid[x-1][y+1] == "alive": #down left
+                liveNeighbourCount += 1
+        return liveNeighbourCount
+    #y=0
+    if y == 0:
+        if grid[x][y+1] == "alive": #down
+            liveNeighbourCount += 1
+        if grid[x+1][y] == "alive": #right
+            liveNeighbourCount += 1
+        if grid[x-1][y] == "alive": #left
+            liveNeighbourCount += 1
+        if grid[x-1][y+1] == "alive": #down left
+            liveNeighbourCount += 1
+        if grid[x+1][y+1] == "alive": #down right
+            liveNeighbourCount += 1
+        return liveNeighbourCount
 
+    #y=max
+    if y == ylength:
+        if grid[x][y-1] == "alive": #up
+            liveNeighbourCount += 1
+        if grid[x+1][y] == "alive": #right
+            liveNeighbourCount += 1
+        if grid[x-1][y] == "alive": #left
+            liveNeighbourCount += 1
+        if grid[x-1][y-1] == "alive": #up left
+            liveNeighbourCount += 1
+        if grid[x+1][y-1] == "alive": #up right
+            liveNeighbourCount += 1
+        return liveNeighbourCount
+
+    #general case
+    if grid[x][y-1] == "alive": #up
+        liveNeighbourCount += 1
+    if grid[x][y+1] == "alive": #down
+        liveNeighbourCount += 1
+    if grid[x+1][y] == "alive": #right
+        liveNeighbourCount += 1
+    if grid[x-1][y] == "alive": #left
+        liveNeighbourCount += 1
+    if grid[x-1][y-1] == "alive": #up left
+        liveNeighbourCount += 1
+    if grid[x+1][y-1] == "alive": #up right
+        liveNeighbourCount += 1
+    if grid[x-1][y+1] == "alive": #down left
+        liveNeighbourCount += 1
+    if grid[x+1][y+1] == "alive": #down right
+        liveNeighbourCount += 1
+    return liveNeighbourCount
+
+def main():
+    xlength = len(grid)
+    ylength = len(grid[0])
+    print(xlength,ylength)
+    for x in range(xlength):
+        for y in range(ylength):
+            if grid[x][y] == "alive":
+                if getLiveNeighbourCount(x,y) < 2:
+                    grid[x][y] = "dead"
+                elif getLiveNeighbourCount(x,y) > 3:
+                    grid[x][y] = "dead"
+            elif grid[x][y] == "dead":
+                if getLiveNeighbourCount(x,y) == 3:
+                    grid[x][y] = "alive"
+
+play = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -67,14 +189,23 @@ while running:
             if event.button == 1:
                 mousex, mousey = pygame.mouse.get_pos()
                 toggleBlock(mousex, mousey)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if play == True:
+                    play = False
+                elif play == False:
+                    play = True
 
     screen.fill("white")
     drawGrid()
 
-    # main loop
+    #main game loop
+
+    if play:
+        main()
 
     # update display
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(10)
 
 pygame.quit()
